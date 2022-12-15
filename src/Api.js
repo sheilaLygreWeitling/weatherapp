05afb5876ea6fad4af5e52bbe2715116760dbb6c
sheilaLygreWeitling <sheilalygreweitling@gmail.com>
2022-12-15 11:04:32 +0100

    add search func for city

---
+++ src/Api.js
@@ -0,0 +1,12 @@
+
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '7752e0c7f2mshff64e3205efe8b5p143ad4jsn28b0cbceb3f1',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+
