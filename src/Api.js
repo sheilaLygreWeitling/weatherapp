d653a215bf32c7cc9ba2f9f09457e46f2b20bb25
sheilaLygreWeitling <sheilalygreweitling@gmail.com>
2022-12-15 11:25:47 +0100

    quickly add env to hide API key

--- src/Api.js
+++ src/Api.js
@@ -1,8 +1,10 @@
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+
 export const geoApiOptions = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': '7752e0c7f2mshff64e3205efe8b5p143ad4jsn28b0cbceb3f1',
+        'X-RapidAPI-Key': `${API_KEY}`,
         'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
     }
 };
