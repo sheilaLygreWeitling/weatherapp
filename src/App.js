e629c75decd97c562445cce0105337dbf4972af6
sheilaLygreWeitling <sheilalygreweitling@gmail.com>
2023-01-06 13:33:43 +0100

    comment weather map

--- src/App.js
+++ src/App.js
@@ -10,25 +10,25 @@ function App() {
 
   const [currentWeather, setCurrentWeather] = useState(null)
   const [forecastWeather, setForecastWeather] = useState(null)
-  const [weatherMap, setWeatherMap] = useState(null)
+  /*   const [weatherMap, setWeatherMap] = useState(null) */
 
   const handleOnSearchChange = (searchData) => {
     const [lat, lon] = searchData.value.split(" ")
-    const temp_new = "temp_new"
+    /*     const temp_new = "temp_new" */
 
 
     const currentWeatherFetch = fetch(`${WEATHER_API_URL}weather?lang=da&lat=${lat}&lon=${lon}&appid=${API_KEY_WEATHER}&units=metric`)
     const forecastWeatherFetch = fetch(`${WEATHER_API_URL}forecast?lang=da&lat=${lat}&lon=${lon}&appid=${API_KEY_WEATHER}&units=metric`)
-    const weatherMapFetch = fetch(`https://tile.openweathermap.org/map/${temp_new}/${100}/${9.501785}/${56.26392}.png?appid=${API_KEY_WEATHER}`)
-
-    Promise.all([currentWeatherFetch, forecastWeatherFetch, weatherMapFetch])
+    /*     const weatherMapFetch = fetch(`https://tile.openweathermap.org/map/${temp_new}/${100}/${9.501785}/${56.26392}.png?appid=${API_KEY_WEATHER}`)
+     */
+    Promise.all([currentWeatherFetch, forecastWeatherFetch, /* weatherMapFetch */])
       .then(async (response) => {
         const weatherResponse = await response[0].json()
         const forecastResponse = await response[1].json()
-        const weatherMapResponse = await response[2].json()
-        setCurrentWeather({ city: searchData.label, ...weatherResponse })
+/*         const weatherMapResponse = await response[2].json()
+ */        setCurrentWeather({ city: searchData.label, ...weatherResponse })
         setForecastWeather({ city: searchData.label, ...forecastResponse })
-        setWeatherMap({ city: searchData.label, ...weatherMapResponse })
+        /*         setWeatherMap({ city: searchData.label, ...weatherMapResponse }) */
       }
       )
       .catch(err => console.error(err))
@@ -42,7 +42,7 @@ function App() {
       <div>
         {currentWeather && <CurrentWeather data={currentWeather} />}
         {forecastWeather && <Forecast data={forecastWeather} />}
-        {weatherMap && <WeatherMap data={weatherMap} />}
+        {/*         {weatherMap && <WeatherMap data={weatherMap} />} */}
       </div>
     </div>
   );
